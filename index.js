65d04fe548760d6066084859c221c37f6db39b47
vikash-kanauja <vikashkanoujia@gmail.com>
2024-04-15 11:02:49 +0530

    optimized code

--- index.js
+++ index.js
@@ -13,79 +13,77 @@ const errorMessage = document.getElementById("error");
 const API_KEY = "82005d27a116c2880c8f0fcb866998a0";
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 
+const toggleElements = (loaderState, weatherBoxState, errorState) => {
+  loader.classList.toggle("hidden", loaderState);
+  weatherbox.classList.toggle("hidden", weatherBoxState);
+  locationError.classList.toggle("hidden", errorState);
+};
+
 // Function to update the UI with weather details
 const showWeatherDetails = (weatherinfo) => {
-  loader.classList.add("hidden");
-  weatherbox.classList.remove("hidden");
-  locationError.classList.add("hidden");
   searchInput.value = "";
   cityLocation.innerText = weatherinfo.name + " ," + weatherinfo.sys.country;
   weatherDescription.innerText = weatherinfo.weather[0].description;
   weatherImg.src = "./images/" + weatherinfo.weather[0].icon + ".png";
   temprature.innerText = (weatherinfo.main.temp - 273.15).toFixed(1) + "°";
   document.body.style.backgroundImage = `url(images/${weatherinfo.weather[0].main}.jpg)`;
+  toggleElements(true,false, true);
 };
 
-const fetchDatafromCoordinates = async (lat,lon) => {
-  try {
-    const url = `${baseUrl}lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => showWeatherDetails(data) 
-    );
-     
-  } catch (error) {
-    showErrorMessage();
-  }
+const fetchDatafromCoordinates = (lat, lon) => {
+    toggleElements(false, true, true);
+  const url = `${baseUrl}lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+   fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      showWeatherDetails(data)
+    })
+    .catch(() => {
+      toggleElements(true, true, false);
+      errorMessage.innerText = "Oops city not found.";
+    });
 };
 
 // Function to fetch weather details based on city name
-const getCityCoordinates = async () => {
-  const cityName = searchInput.value.trim();
-  loader.classList.remove("hidden");
-  weatherbox.classList.add("hidden");
-  locationError.classList.add("hidden");
-
-  if (cityName === "") {
-    loader.classList.add("hidden");
-    weatherbox.classList.add("hidden");
-    locationError.classList.add("hidden");
+const getCityCoordinates =  () => {
+    const cityName =searchInput.value;
+  if (!cityName) {
+    console.log("return");
     return;
-  }
-
+  }else{
+      searchInput.value.trim();
+console.log(cityName);
   const url = `${baseUrl}q=${cityName}&appid=${API_KEY}`;
 
   // Fetch weather data from the API
-  await fetch(url)
+//   await 
+    fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      fetchDatafromCoordinates(data.coord.lat,data.coord.lon);
+      fetchDatafromCoordinates(data.coord.lat, data.coord.lon)
+      toggleElements(false, true, true)
     })
     .catch(() => {
-      showErrorMessage();
+    //   showErrorMessage();
+    errorMessage.innerText = "Oops city not found.";
+      toggleElements(true, true, false);
+      
     });
+  }
 };
 
 // Function to fetch weather details based on user's current location
 const getUserCoordinates = () => {
+  toggleElements(true, true, true);
   navigator.geolocation.getCurrentPosition(
-    async (position) => {
+    // async
+     (position) => {
       const { latitude, longitude } = position.coords;
-      loader.classList.remove("hidden");
-      weatherbox.classList.add("hidden");
-      locationError.classList.add("hidden");
-      fetchDatafromCoordinates(latitude,longitude)
+      fetchDatafromCoordinates(latitude, longitude)
     },
     (error) => {
-      if (error.code === error.PERMISSION_DENIED) {
-        weatherbox.classList.add("hidden");
-        locationError.classList.remove("hidden");
-        loader.classList.add("hidden");
-        errorMessage.innerText = "Location not available";
-      } else {
-        weatherbox.classList.add("hidden");
-        locationError.classList.add("hidden");
-      }
+      toggleElements(true, true, false);
+      errorMessage.innerText = "Location not available";
     }
   );
 };
@@ -93,32 +91,25 @@ const getUserCoordinates = () => {
 // Disable search button if input is empty
 searchButton.disabled = true;
 searchInput.addEventListener("change", () => {
-  if (searchInput.value === "") {
-    searchButton.disabled = true;
-  } else {
-    searchButton.disabled = false;
-  }
+  searchButton.disabled = searchInput.value === "";
 });
 
 // Fetch weather data based on user's current location
 getUserCoordinates();
 
-const showErrorMessage =() =>{
-  loader.classList.add("hidden");
-  weatherbox.classList.add("hidden");
-  locationError.classList.remove("hidden");
-  errorMessage.innerText = "Oops city not found.";
-}
-
 // Event listener for search button click
 searchButton.addEventListener("click", getCityCoordinates);
 
 // Execute a function when the user presses a key on the keyboard
-searchInput.addEventListener("keypress", function(event) {
+searchInput.addEventListener("keypress", function (event) {
 
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
     event.preventDefault();
     getCityCoordinates()
-    }
+  }else if(searchInput.value =="" && event.keyCode==32){
+    event.preventDefault();
+    return;
+  }
 });
+
